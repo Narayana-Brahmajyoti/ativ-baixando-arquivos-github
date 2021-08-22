f80993f690ff289659961b6832e6ea33d9a57042
narayana <nbrahmajyoti@gmail.com>
2021-08-21 23:41:30 -0300

    meu primeiro commit

---
+++ app.js
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Atividade Introdução a Informática</title>
+<h1 style="color:GhostWhite;" align="center">Atividade-Introdução a Informática: adicionando arquivos ao repositório</h1>
+</head>
+<body style="background-color:DarkViolet;">
+<p style="color:GhostWhite;">Teste</p>
+</body>
+</html>
